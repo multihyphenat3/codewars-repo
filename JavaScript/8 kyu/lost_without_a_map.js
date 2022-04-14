e9d178f05f148dc8d9c777cd337c4032c2ea5484
multihyphenat3 <99143164+multihyphenat3@users.noreply.github.com>
2022-04-13 19:50:43 -0500

    completed codewars 8kyu lost without a map.

---
+++ JavaScript/8 kyu/lost_without_a_map.js
@@ -0,0 +1,10 @@
+// Given an array of integers, return a new array with each value doubled.
+
+function maps(x){
+    let newArr = [];
+     for(let i = 0; i < x.length; i++){
+       newArr.push(x[i] * 2);
+     }
+     return newArr
+   }
+   
