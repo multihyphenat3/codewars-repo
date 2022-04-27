077ff6bd3acbe24a79f3efa6b4583c20fecc3a03
multihyphenat3 <99143164+multihyphenat3@users.noreply.github.com>
2022-04-27 00:00:27 -0500

    completed codewars 8kyu

---
+++ JavaScript/8 kyu/new_js.js
@@ -0,0 +1,9 @@
+function howMuchWater(water, load, clothes){
+    if (clothes > 2*load){
+      return 'Too much clothes'
+    }else if (clothes < load){
+      return 'Not enough clothes'
+    }else{
+      return +((water)*1.1**(clothes-load)).toFixed(2)
+    }
+  }
