7bff94acd16faf4614e0b6c8cf12d081d9a48cd8
multihyphenat3 <larryellis1996@gmail.com>
2022-05-07 13:56:04 -0500

    completed codewars 8kyu 'plural' which allows the user to input a number and have a return value of true or false based on whether or not the number is plural.

---
+++ JavaScript/8 kyu/plural.js
@@ -0,0 +1,10 @@
+//Created a simple function that returns the input as true or false depending upon whether its plural or not
+function plural(n) {
+    if (n==1) return false
+    if (n>1){
+      return true
+    }else{
+      return true
+    }
+    }
+  
