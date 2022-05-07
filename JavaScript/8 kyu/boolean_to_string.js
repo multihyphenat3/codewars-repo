3ca1fdcceab17eb18102389663acf54f2580cb2c
multihyphenat3 <larryellis1996@gmail.com>
2022-05-07 09:26:46 -0500

    Completed codewars 8kyu converting a boolean to a string.

---
+++ JavaScript/8 kyu/boolean_to_string.js
@@ -0,0 +1,5 @@
+// Implement a function which convert the given boolean value into its string representation.
+// all you did was slap the toString method onto what was passed into the function!
+function booleanToString(b){
+    return b.toString();
+  }
