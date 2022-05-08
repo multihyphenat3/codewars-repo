dc7e561d40984b7e3cda61d54e711b139a2bdfb4
multihyphenat3 <larryellis1996@gmail.com>
2022-05-08 13:15:28 -0500

    completed 8kyu codewars function to double integers.

---
+++ JavaScript/8 kyu/doubling_integers.js
@@ -0,0 +1,9 @@
+//Function for doubling an integer as quick as possible
+function doubleInteger(i) {
+    return i *=2;
+  }
+  /* 
+  Input: i doubled
+  Output: i *=2 
+  Logic: multiply parameter by 2 
+  */
