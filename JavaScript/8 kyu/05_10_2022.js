27bb3962620ff7fa7ff968298b87bcf670ec1ea9
multihyphenat3 <larryellis1996@gmail.com>
2022-05-10 11:45:54 -0500

    completed codewars 8kyu checking two parameters to see if I have enough items for the other.

---
+++ JavaScript/8 kyu/05_10_2022.js
@@ -0,0 +1,8 @@
+/*create a function that checks to see if the 
+player will live or die based on the number of
+bullets they brought to defeat the # of dragons!
+It takes 2 bullets to defeat each dragon
+*/
+function hero(bullets, dragons){
+    return (bullets/2 >= dragons)
+    }
