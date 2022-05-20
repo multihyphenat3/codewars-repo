c798834064dea7d640b89a74df024bc0cbed15e8
multihyphenat3 <larryellis1996@gmail.com>
2022-05-19 23:57:53 -0500

    completed codewars 8kyu regarding pets ata zoo using switch case statements.

---
+++ JavaScript/8 kyu/05_19_2022.js
@@ -0,0 +1,20 @@
+switch (pet) {
+    case "lizard":
+      console.log("I own a lizard");
+      break;
+    case "dog":
+      console.log("I own a dog");
+      break;
+    case "cat":
+      console.log("I own a cat");
+      break;
+    case "snake":
+      console.log("I own a snake");
+      break;
+    case "parrot":
+      console.log("I own a parrot");
+      break;
+    default:
+      console.log("I don't own a pet");
+      break;
+  }
