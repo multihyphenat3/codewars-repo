0d98c0fe1197424af60f4502a722738b3da7efeb
multihyphenat3 <larryellis1996@gmail.com>
2022-05-20 15:58:02 -0500

    completed codewars regarding counting the number of hoops a player is to score, if the player scores greater than 10 vs less than 10.

---
+++ JavaScript/8 kyu/05_20_22.js
@@ -0,0 +1,11 @@
+function hoopCount (n) {
+    if (n < 10 ) {
+      return "Keep at it until you get it" ;
+    }
+    else if (n >10 ) {
+      return "Great, now move on to tricks" ;
+    }
+    else {
+      return "Great, now move on to tricks" ;
+    }
+ }
