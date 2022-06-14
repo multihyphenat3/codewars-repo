70caf1693da7af826b0c66a1252c57b63cdd346f
multihyphenat3 <larryellis1996@gmail.com>
2022-06-14 11:29:00 -0500

    completed codewars regarding else if statements.

---
+++ JavaScript/8 kyu/06_14_22.js
@@ -0,0 +1,16 @@
+function peopleWithAgeDrink(old) {
+    let age = 22
+    if (age === 13){
+      return "drink toddy"
+    }else if (age === 17){
+      return "drink coke"
+    }else if(age === 18){
+      return "drink beer"
+    }else if(age === 20){
+      return "drink beer"
+    }else if(age === 22){
+      return "drink whisky"
+    }else{
+      return age
+    }
+  };
