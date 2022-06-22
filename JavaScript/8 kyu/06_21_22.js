8f58c17eac10be79d64a60a4b0e74ddea147432a
multihyphenat3 <larryellis1996@gmail.com>
2022-06-21 23:53:34 -0500

    created function that checks parameters for their casing.

---
+++ JavaScript/8 kyu/06_21_22.js
@@ -0,0 +1,11 @@
+function sameCase(a, b){
+    let alphabet = ['abcdefghijklmnopqrstuvwxyz']
+    for (let i = 0; i <= alphabet.length; i++){
+       if (a = b){
+      return 1
+    }else if(a || b != alphabet.length){
+      return -1
+    }else if(a || b != alphabet.length){
+      return -1
+    }
+  }
